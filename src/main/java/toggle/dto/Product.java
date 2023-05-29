102226a9f0ae7f2da2eb5d226b61e8dce886c768
igorp <igorprohorchenko@gmail.com>
2023-05-29 15:51:23 +0300

    ADD product & product service

---
+++ src/main/java/toggle/dto/Product.java
@@ -0,0 +1,17 @@
+package toggle.dto;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Product {
+    int id;
+    String name;
+    double price;
+}
---
+++ src/main/java/toggle/service/InventoryService.java
@@ -0,0 +1,19 @@
+package toggle.service;
+
+import org.springframework.stereotype.Service;
+import toggle.dto.Product;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Service
+public class InventoryService {
+
+    public List<Product> getAllProducts() {
+        return Stream.of(new Product(1, "mobile", 50000),
+                new Product(3, "watch", 14999),
+                new Product(4, "glass", 999)
+        ).collect(Collectors.toList());
+    }
+}
